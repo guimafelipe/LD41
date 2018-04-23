1de39c25c47b4921086599644387a6f79e115798
Felipe Guimaraes <guima.felipec@gmail.com>
2018-04-23 00:16:57 -0300

    customers now have emotions!

--- Scripts/Customer.gd
+++ Scripts/Customer.gd
@@ -11,22 +11,36 @@ signal wasFeed
 signal fedWrong
 signal diedFromHunger
 
-var min_time = [15.0, 12.0, 10.0, 8.0]
-var max_time = [25.0, 23.0, 19.0, 15.0]
+var min_time #= [15.0, 12.0, 10.0, 8.0]
+var max_time #= [25.0, 23.0, 19.0, 15.0]
+
+var faces = [
+	preload("res://Art/Emoticons/emoticon1.png"),
+	preload("res://Art/Emoticons/emoticon2.png"),
+	preload("res://Art/Emoticons/emoticon3.png"),
+	preload("res://Art/Emoticons/emoticon4.png"),
+	preload("res://Art/Emoticons/emoticon5.png"),
+	preload("res://Art/Emoticons/emoticon6.png"),
+]
+
+var was_fed = false
 
 func _ready():
 	#print(self is foodType)
+	_get_data()
+	pass
+
+func _get_data():
 	min_time = GlobalData.min_time
 	max_time = GlobalData.max_time
-	pass
 
 func setDifficulty(difficulty): #that is the function that actually start
 	randomize()
+	_get_data()
 	var pat_range = max_time[difficulty] - min_time[difficulty]
 	patience = min_time[difficulty] + randf()*pat_range
 	$Patience.wait_time = patience
 	myFood = randi()%3
-	
 	startTimer()
 	pass
 
@@ -35,7 +49,21 @@ func _process(delta):
 	var remainingTime = $Patience.time_left
 	#mudar a cor do mesh instance
 	updateMesh(remainingTime)
+	updateFace(remainingTime)
 	pass
+
+func updateFace(remainingTime):
+	if was_fed:
+		return
+	if(remainingTime < 5.0):
+		$Face.texture = faces[3]
+		return
+	if(remainingTime < 10.0):
+		$Face.texture = faces[2]
+		return
+	if(remainingTime < 15.0):
+		$Face.texture = faces[1]
+	$Face.texture = faces[0]
 
 func updateMesh(remaigingTime):
 	$MeshInstance.updateMesh(remaigingTime)
@@ -52,17 +80,19 @@ func _on_Patience_timeout():
 func _on_Customer_body_entered( body ):
 	print(body is foodType)
 	if(body is foodType):
-#		print("Chocou com a comida")
+		was_fed = true
 		# checar se é a comida certa
 		if(body.foodClass == myFood):
 #			print("Comida certa")
 			# ficar feliz
 			#se retirar do restaurante
 			emit_signal("wasFeed")
+			$Face.texture = faces[4]
 			body.queue_free()
 			queue_free() #aqui pode ter uma animaçãozinha antes
 		else:
 			body.queue_free()			
 #			print("Comida errada")
+			$Face.texture = faces[5]
 			emit_signal("fedWrong")
 			#perdeu o jogo =/
